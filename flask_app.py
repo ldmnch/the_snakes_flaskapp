54d76a24089cc9a26384b17ccb40d53d2bf28faf
Franco Bastida <42223533+francobastida@users.noreply.github.com>
2025-03-10 15:37:43 +0100

    Added base layout and buttons

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+@app.route("/home")
+def default_home():
+    return render_template('base.html')
+
+@app.route("/game")
+@app.route("/index")
+@app.route("/start")
+@app.route("/maze")
+def maze():
     return render_template("index.html")
 
 if __name__ == "__main__":
